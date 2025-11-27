5031a74ab997882383a55f2ea60488d2b587e5f0
NaveenChandraBairaju <naveenraj1315@gmail.com>
2025-11-27 11:58:20 +0530

    session Name changed

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 console.log("Welcome to git session2")
-let sessionName = "Session 2"
+let sessionName = "Matromorphosis - Session  - batch 2"
 let topicName = "Matromorphosis"
 let trainerName = "Sudha"
 let duration = "3 hours"
 console.log(sessionName)
+
 
