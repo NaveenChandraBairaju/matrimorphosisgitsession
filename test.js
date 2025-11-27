1422a35e92e83376acc692940dedbc619e33f016
NaveenChandraBairaju <naveenraj1315@gmail.com>
2025-11-27 11:54:12 +0530

    Time duration changes

--- test.js
+++ test.js
@@ -2,5 +2,6 @@ console.log("Welcome to git session2")
 let sessionName = "Session 2"
 let topicName = "Matromorphosis"
 let trainerName = "Sudha"
-let duration = "2 hours"
+let duration = "2.5 hours"
+console.log(sessionName)
 
